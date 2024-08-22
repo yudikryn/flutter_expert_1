4f68b3217b83a1c40eca189f031b21f40f04423d
Yudi Kurniyawan <yudikryn@Yudis-MacBook-Air.local>
2024-08-22 19:55:01 +0700

    add usecase test

---
+++ test/domain/usecases/tv/get_tv_airing_today_test.dart
@@ -0,0 +1,29 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/domain/entities/tv.dart';
+import 'package:ditonton/domain/usecases/tv/get_tv_airing_today.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../helpers/test_helper.mocks.dart';
+
+void main() {
+  late GetTvAiringToday usecase;
+  late MockTvRepository mockTvRepository;
+
+  setUp(() {
+    mockTvRepository = MockTvRepository();
+    usecase = GetTvAiringToday(mockTvRepository);
+  });
+
+  final tTv = <Tv>[];
+
+  test('should get list of tv from the repository', () async {
+    // arrange
+    when(mockTvRepository.getAiringTodayTv())
+        .thenAnswer((_) async => Right(tTv));
+    // act
+    final result = await usecase.execute();
+    // assert
+    expect(result, Right(tTv));
+  });
+}

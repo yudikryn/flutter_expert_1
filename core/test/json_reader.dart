3d126705f87d327161aa345f07f6087fe03d2eff
Yudi Kurniyawan <yudikryn@Yudis-MacBook-Air.local>
2024-12-16 23:16:35 +0700

    fix call test using subdirectory

--- core/test/json_reader.dart
+++ core/test/json_reader.dart
@@ -1,9 +1,17 @@
 import 'dart:io';
 
 String readJson(String name) {
-  var dir = Directory.current.path;
-  if (dir.endsWith('/test')) {
-    dir = dir.replaceAll('/test', '');
+  try {
+    var dir = Directory.current.path;
+    if (dir.endsWith('/test')) {
+      dir = dir.replaceAll('/test', '');
+    }
+    return File('$dir/test/$name').readAsStringSync();
+  } catch (e) {
+    var dir = Directory.current.path;
+    if (dir.endsWith('/core/test')) {
+      dir = dir.replaceAll('/core/test', '');
+    }
+    return File('$dir/core/test/$name').readAsStringSync();
   }
-  return File('$dir/test/$name').readAsStringSync();
 }
